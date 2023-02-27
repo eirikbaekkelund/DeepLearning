3a81e0740fee6c352abc18e967aa780d11a7353c
Eirik Aalstad Baekkelund <89584092+eirikbaekkelund@users.noreply.github.com>
2023-02-27 18:57:52 +0000

    clean up + docstrings

--- cw1-pt/task1/task1a.py
+++ cw1-pt/task1/task1a.py
@@ -3,15 +3,37 @@ from task import generate_data, polynomial_func
 import matplotlib.pyplot as plt
 
 def ridge_loss(y, t, w, lmbda):
+    """ 
+    Ridge loss to emphasize the importance of the weights and powers of the polynomial.
+    
+    Args:
+        y (torch.Tensor): Predicted values.
+        t (torch.Tensor): Target values.
+        w (torch.Tensor): Weights of the polynomial.
+        lmbda (float): Regularization parameter.
+    Returns:
+        torch.Tensor: Ridge loss.
+    """
     return torch.mean(torch.square((y - t))) + lmbda * torch.norm(w, p=2)
 
-def fit_polynomial_sgd(x, t, M, lr, lmbda=0.1, batch_size=5):
+def fit_polynomial_sgd_M(x, t, M, lr, lmbda=0.1, batch_size=5):
+    """ 
+     Minibatch Stochastic Gradient Descent to fit a polynomial of degree M to the data.
+    Args:
+        x (torch.Tensor): Input data.
+        t (torch.Tensor): Input labels.
+        M (int): Degree of the polynomial.
+        lr (float): Learning rate.
+        lmbda (float): Regularization parameter.
+        batch_size (int): Batch size.
+    Returns:
+        torch.Tensor: Weights of the polynomial.
+    """
     # Initialize parameters
     w = torch.randn(M+1, requires_grad=True)
+    
     optimizer = torch.optim.SGD([w], lr=lr)   
-    
     best_loss = torch.inf    
-    
     n_batches = len(x) // batch_size
 
     for _ in range(200):
@@ -36,6 +58,15 @@ def fit_polynomial_sgd(x, t, M, lr, lmbda=0.1, batch_size=5):
     return best_w, best_loss
 
 def print_learned_M(M_range=torch.arange(0,6), lrs=[1e-2, 1e-3, 1e-5, 1e-8, 1e-10, 1e-13], lmbdas = [100], batch_size=20):
+    """
+    Prints the report of the weights of the polynomial for different degrees M.
+
+    Args:
+        M_range (torch.Tensor): Range of degrees M.
+        lrs (list): List of learning rates.
+        lmbdas (list): List of regularization parameters.
+        batch_size (int): Batch size.
+    """
     x_train, t_train, x_test, t_test = generate_data(100)
     print('\n\n')
     print(f"\t\t LEARNING DEGREE M | Batch Size: {batch_size} | RIDGE REGRESSION IN SGD")
@@ -52,21 +83,21 @@ def print_learned_M(M_range=torch.arange(0,6), lrs=[1e-2, 1e-3, 1e-5, 1e-8, 1e-1
         best_lr = torch.inf
    
         for lmbda in lmbdas:
-            w, _ = fit_polynomial_sgd(x_train, t_train, M, lr, lmbda, batch_size)
+            w, _ = fit_polynomial_sgd_M(x_train, t_train, M, lr, lmbda, batch_size)
             loss = ridge_loss(polynomial_func(x_test, w), t_test, w, lmbda)
             if loss < best_loss:
                 best_loss = loss
                 best_lmbda = lmbda
                 best_lr = lr
     
-        w, _ = fit_polynomial_sgd(x_train, t_train, M, best_lr, best_lmbda, batch_size)
+        w, _ = fit_polynomial_sgd_M(x_train, t_train, M, best_lr, best_lmbda, batch_size)
         loss = ridge_loss(polynomial_func(x_test, w), t_test, w, best_lmbda)
         
         if best_loss < best_loss_M:
             best_loss_M = best_loss
             best_learned_M = M
             best_weights_M = w
-            # best_lr_M = best_lr
+
         if M == 4:
 
             print(f"| {M} | {best_lmbda} \t | {best_lr} \t\t | {round(loss.detach().numpy().tolist(),2)} \t\t |  {[round(weight, 2) for weight in w.detach().numpy().tolist()]}")
@@ -79,11 +110,11 @@ def print_learned_M(M_range=torch.arange(0,6), lrs=[1e-2, 1e-3, 1e-5, 1e-8, 1e-1
     print('\n')
     print('\n')
     
-    # plt.title('Test vs Prediction Scatter Plot')
-    # plt.scatter(x_train, t_train, marker = 'o', label='true values')
-    # plt.scatter(x_train, polynomial_func(x_train, best_weights_M).detach(), marker = 'x', label='predicted values')
-    # plt.legend(loc='best')
-    # plt.show()
+    plt.title('Test vs Prediction Scatter Plot')
+    plt.scatter(x_train, t_train, marker = 'o', label='true values')
+    plt.scatter(x_train, polynomial_func(x_train, best_weights_M).detach(), marker = 'x', label='predicted values')
+    plt.legend(loc='best')
+    plt.show()
 
 if __name__ == "__main__":
     print_learned_M(batch_size=20)
--- cw1-pt/task2/task.py
+++ cw1-pt/task2/task.py
@@ -1,24 +1,89 @@
 import warnings
 with warnings.catch_warnings():
     warnings.filterwarnings("ignore", category=UserWarning)
-    # import os
-    # os.system('sudo ln -s /usr/lib/x86_64-linux-gnu/libjpeg.so.8 /usr/lib/x86_64-linux-gnu/libjpeg.so.62')
-    from torchvision.models import resnet50 as resnet
+
+    from torchvision.models import resnet50 
     import torch
     import torch.nn as nn
     import torchvision
     import torchvision.transforms as transforms
+    from PIL import Image
+    import os
 
-    # Creating a class mixup inheriting
-    class mixup(torch.nn.Module):
+    def load_CIFAR10_data(n_samples=1000, prop_train=0.8):
+        """ 
+        Loads the training and test data.
+        Args:
+            num_pics (int): Number of pictures to load.
+        Returns:
+            torch.utils.data.DataLoader: Training data loader.
+            torch.utils.data.DataLoader: Test data loader.
+        """
+        
+        print('==> Preparing data...')
+
+        transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+        trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform)
+        testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform)
+
+        
+        n_train = int(n_samples*prop_train)
+        n_test = int(n_samples*(1 - prop_train) + 1)
+
+        trainset.data = trainset.data[:n_train]
+        trainset.targets = trainset.targets[:n_train]
+
+        testset.data = testset.data[:n_test]
+        testset.targets = testset.targets[:n_test]
+        # one hot encode targets
+        trainset.targets = torch.nn.functional.one_hot(torch.tensor(trainset.targets), num_classes=10)
+        testset.targets = torch.nn.functional.one_hot(torch.tensor(testset.targets), num_classes=10)
+        
+        trainloader = torch.utils.data.DataLoader(trainset, batch_size=16, shuffle=True, num_workers=2)
+        test_loader = torch.utils.data.DataLoader(testset, batch_size=36, shuffle=False, num_workers=2)
+        
+        print('==> Done!')
+
+        return trainloader, test_loader
+    
+    def montage_mixup_png(data, n_rows=4, n_cols=4):
+        """ 
+        Creates a montage of the images in the data.
+        Args:
+            data (torch array): Data to create montage from.
+            n_rows (int): Number of rows in the montage.
+            n_cols (int): Number of columns in the montage.
+        Returns:
+            PIL image: Montage of the images in the data.
+        """
+        assert data.shape[0] == n_rows * n_cols, "Number of images must be equal to n_rows * n_cols"
+        
+        # Create a new image with a size that can fit all the images
+        new_im = Image.new('RGB', (data.shape[2] * n_cols, data.shape[3] * n_rows))
+    
+        # Paste the images into the new image
+        for i in range(n_rows):
+            for j in range(n_cols):
+                # get image
+                im = data[i*4 + j, :, :, :]
+                # im = im.permute(1, 2, 0)
+                im = transforms.ToPILImage()(im)
+                new_im.paste(im, (j * data.shape[2], i * data.shape[3]))
+        
+        # Save the new image within the same folder as running file
+        path = os.path.dirname(os.path.abspath(__file__))
+        new_im.save( path + '/mixup.png')
+        return new_im
+    
+    class MixUp(torch.nn.Module):
         """ 
         Mixup class that inherits from torch.nn.Module.
         Args:
             sampling_method (int): Sampling method to use. 1 for Beta distribution and 2 for uniform distribution.
             resnet (torchvision model): Resnet model to use.
         """
-        def __init__(self, sampling_method, resnet=resnet):
-            super(mixup, self).__init__()
+        def __init__(self, sampling_method, resnet50=resnet50):
+            super(MixUp, self).__init__()
             assert sampling_method in [1, 2], "Sampling method must be 1 or 2"
             
             if sampling_method == 1:
@@ -28,12 +93,27 @@ with warnings.catch_warnings():
                 uniform_range = torch.arange(0, 1, 0.01)
                 self.sample_func = torch.distributions.Uniform(uniform_range[0], uniform_range[-1])
             
-            self.model = resnet(pretrained=True)
+            self.network = resnet50(pretrained=False, progress=True)
+            # replace last FC layer and add softmax layer
+            self.network.fc = nn.Sequential(
+                nn.Linear(2048, 10),
+                nn.Softmax(dim=1)
+            )
+
+            for name, params in self.network.named_parameters():
+            
+                if name.startswith('fc'):
+                    params.requires_grad = True
+            
+                else:
+                    params.requires_grad = True
+            
             self.loss = nn.functional.cross_entropy
 
-        def forward(self, x, y):
+        def forward_mixup(self, x, y):
             """ 
-            Performs mixup on the input data and labels.
+            Performs mixup algorithm on the input data and labels.            
+
             Args:
                 x (torch array): Input data.
                 y (torch array): Input labels.
@@ -48,126 +128,104 @@ with warnings.catch_warnings():
             idx = torch.randperm(x.size()[0])
             
             # Perform mixup
-            x_hat = lam * x + (1 - lam) * x[idx, :]
-            # y_hat = lam * y + (1 - lam) * y[idx, :]
-            y_hat = lam * y + (1 - lam) * y.gather(0, idx.expand(-1, y.size(1), -1, -1))
-
-            
+            x_hat = lam * x + (1 - lam) * x[idx, :, :, :]
+            y_hat = torch.tensor(lam * y + (1 - lam) * y[idx], dtype= torch.float32)
+        
             return x_hat, y_hat
         
-        
-        
-    def train(model, train_loader, optimizer, device):
+    def train(sampling_method, train_loader, device, n_epochs=10, lr=0.001, momentum=0.01):
         """ 
         Trains the model on the training data.
+        
         Args:
             model (torchvision model): Model to train.
             train_loader (torch.utils.data.DataLoader): Training data loader.
             optimizer (torch.optim): Optimizer to use.
             device (torch.device): Device to use.
+        Returns:
+            torchvision model: Trained model.
         """
-        model.train()
-        for batch_idx, (data, target) in enumerate(train_loader):
-            data, target = data.to(device), target.to(device)
+
+        model = MixUp(sampling_method)
+        optimizer = torch.optim.SGD(model.parameters(), lr=lr, momentum=momentum)
+        
+        print('==> Starting training...')
+
+        for epoch in range(n_epochs):
+            for batch_idx, (data, target) in enumerate(train_loader):
+
+                data, target = data.to(device), target.to(device)        
+                data, target = model.forward_mixup(data, target)
+                
+                if epoch == 0 and batch_idx == 0:
+                    montage_mixup_png(data)
+
+                optimizer.zero_grad()
+                output = model.network.forward(data)
+                loss = model.loss(output, target)
+                loss.backward()
+                optimizer.step()
             
-            print('data shape: ', data.shape)
-            print('target shape: ', target.shape)
-            print(target)
-            # Perform mixup
-            data, target = model(data, target)
-            
-            optimizer.zero_grad()
-            output = model.model(data)
-            loss = model.loss(output, target)
-            loss.backward()
-            optimizer.step()
+#            print('gradients: ', model.network.fc[0].weight.grad[0, 0:5])   
+            print('Epoch: {} \tLoss: {:.6f}'.format(epoch + 1, loss.item()))
+        
+        print('==> Finished training...')
+        
+        return model
 
     def test(model, test_loader, device):
         """ 
         Tests the model on the test data.
+        
         Args:
             model (torchvision model): Model to test.
             test_loader (torch.utils.data.DataLoader): Test data loader.
             device (torch.device): Device to use.
         """
-        model.eval()
+        print('==> Testing model...')
+
         test_loss = 0
         correct = 0
+        
         with torch.no_grad():
+            
             for data, target in test_loader:
                 data, target = data.to(device), target.to(device)
-                output = model.model(data)
+                output = model.network.forward(data)
                 test_loss += model.loss(output, target, reduction='sum').item()  # sum up batch loss
                 pred = output.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
-                correct += pred.eq(target.view_as(pred)).sum().item()
+                target = target.argmax(dim=1, keepdim=True)
+                correct = torch.mean((pred.squeeze() == target.squeeze()).float())
 
         test_loss /= len(test_loader.dataset)
 
         print('Test set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%'')'.format(test_loss, correct, len(test_loader.dataset)))
-
-    def load_data(num_pics=16):
-        """ 
-        Loads the training and test data.
-        Args:
-            num_pics (int): Number of pictures to load.
-        Returns:
-            torch.utils.data.DataLoader: Training data loader.
-            torch.utils.data.DataLoader: Test data loader.
-        """
-
-        print('==> Preparing data...')
-
-        transform = transforms.Compose(
-            [transforms.ToTensor(),
-            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
-        
-        trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
-                                                download=True, transform=transform)
-        
-        trainset.data = trainset.data[:num_pics]
-        trainset.targets = trainset.targets[:num_pics]
-        
-        trainloader = torch.utils.data.DataLoader(trainset, batch_size=4,
-                                                shuffle=True, num_workers=2)
-        
-        testset = torchvision.datasets.CIFAR10(root='./data', train=False,
-                                            download=True, transform=transform)
-        
-        testset.data = testset.data[:num_pics]
-        testset.targets = testset.targets[:num_pics]
-        
-        testloader = torch.utils.data.DataLoader(testset, batch_size=4,
-                                                shuffle=False, num_workers=2)
-        
-        return trainloader, testloader
-
-    def fix_libjpeg8():
-        """ 
-        Fixes a bug in the libjpeg8 library.
-        """
-        import os
-        os.system('sudo ln -s /usr/lib/x86_64-linux-gnu/libjpeg.so.8 /usr/lib/x86_64-linux-gnu/libjpeg.so.62')
+        print('==> Finished testing...')
 
     if __name__ == "__main__":
-        # Fix libjpeg8 bug
-        fix_libjpeg8()
-        print('==> Starting training...')
+
+        print('\n')
+                
         # Set device
         device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         print("Using device:", device)
+        
+        print('\n')
         # Load data
-        train_loader, test_loader = load_data()
+        train_loader, test_loader = load_CIFAR10_data()
+
+        print('\n')
         print('Number of training samples:', len(train_loader.dataset))
-        # Create model
-        model = mixup(sampling_method=1).to(device)
+        print('Number of test samples:', len(test_loader.dataset))        
         
-        # Create optimizer
-        optimizer = torch.optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
-        
+        print('\n')
         # Train model
-        for epoch in range(1, 10 + 1):
-            train(model, train_loader, optimizer, device)
-            test(model, test_loader, device)
-            
+        model = train(sampling_method=1, train_loader=train_loader,device=device)
+
+        print('\n')
+
+        test(model, test_loader, device)
+        
+        print('\n')
         # Save model
         torch.save(model.state_dict(), "model.pt")

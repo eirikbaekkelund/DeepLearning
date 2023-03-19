fe6e915e9f8090f8c64831260397f96803c4354b
Eirik Aalstad Baekkelund <89584092+eirikbaekkelund@users.noreply.github.com>
2023-03-19 11:39:21 +0000

    update task3

--- cw1-pt/task3/task.py
+++ cw1-pt/task3/task.py
@@ -1,124 +1,140 @@
-from task2.task import ResNet, MixUp, load_cifar10_data
-import numpy as np
+from classes import MixUp
+from utils import load_cifar10_train_val_test
 import torch
-from torch.utils.data import Subset, DataLoader
+from torch.autograd import Variable
+import time
+import os
 
-class Ablation(ResNet):
-    def __init__(self, optimizer, sampling_method):
-        super(Ablation).__init__()
-        assert sampling_method in [1,2], "Sampling method must be either 1 or 2. 1 - Beta sampling, 2 - Uniform sampling (for mixup)"
-        assert optimizer in ['sgd', 'adam'], "Optimizer must be either 'sgd' or 'adam'"
+class Ablation(MixUp):
+    """
+    Class for comparing adam vs. sgd 
+    Includes a validation tester as compared with task2.
+    """
+    def __init__(self, optimizer, num_classes):
+        super().__init__(optimizer=optimizer)
+        self.num_classes = num_classes
+  
+    def f1_score(self, targets, true_positives, false_positives, false_negatives):
+        """
+        Calculates the F1 score for a batch of predictions.
+        """
+        epsilon = 1e-7
+        precision = true_positives / (true_positives + false_positives + epsilon)
+        recall = true_positives / (true_positives + false_negatives + epsilon)
+        f1_scores = 2 * precision * recall / (precision + recall + epsilon)
+
+        weights = torch.zeros(self.num_classes)
+        for i in range(self.num_classes):
+            weights[i] = (targets == i).sum().item()
+
+        weights /= weights.sum()
+        val_f1 = (f1_scores * weights).sum().item()
+        return val_f1
+
         
-        if optimizer == 'sgd':
-            self.optimizer = torch.optim.SGD(self.resnet50.parameters(), lr=0.01, momentum=0.9)
+
+    def val_model(self, val_loader):
+        """ 
+        Tests the model on the validation data.
+        
+        Args:
+            model (torchvision model): Model to test.
+            val_loader (torch.utils.data.DataLoader): Validation data loader.
+        Returns:
+            val_loss (float): Validation loss.
+            val_acc (float): Validation accuracy.
+            val_f1 (float): Validation F1 score.
+        """
+        val_loss = 0
+        correct = 0
+        total = 0
+  
+        true_positives = torch.zeros(self.num_classes)
+        false_positives = torch.zeros(self.num_classes)
+        false_negatives = torch.zeros(self.num_classes)
+        
+        self.resnet50.eval()
+        
+        with torch.no_grad():
+            for data, targets in val_loader:
+
+                inputs, targets = Variable(data), Variable(targets)
+                outputs = self.resnet50(inputs)
+                loss = self.criterion(outputs, targets)
+
+                val_loss += loss.item()
+                _, predicted = torch.max(outputs.data, 1)
+                total += targets.size(0)
+                correct += predicted.eq(targets.data).cpu().sum()
+
+                # Update true positives, false positives, and false negatives for F1 score calculation
+                for i in range(self.num_classes):
+                    true_positives[i] += ((predicted == i) & (targets == i)).sum().item()
+                    false_positives[i] += ((predicted == i) & (targets != i)).sum().item()
+                    false_negatives[i] += ((predicted != i) & (targets == i)).sum().item()
+
+            val_f1 = self.f1_score(targets=targets,
+                                   true_positives=true_positives, 
+                                   false_positives=false_positives, 
+                                   false_negatives=false_negatives)
+            
+            val_loss = val_loss / len(val_loader)
+            val_acc = 100. * (correct / total)
+            
+        return val_loss, val_acc.item(), val_f1
+        
+    def train(self, train_loader, val_loader, test_loader, epochs=10):
+        """
+        Trains and tests the model.
+
+        Args:
+            model (torchvision model): Model to train and test.
+            train_loader (torch.utils.data.DataLoader): Training data loader with mixup.
+            test_loader (torch.utils.data.DataLoader): Test data loader.
+            epochs (int): Number of epochs to train for.
+        Returns:
+            train_losses (list): List of training losses.
+            train_accs (list): List of training accuracies.
+            test_losses (list): List of test losses.
+            test_accs (list): List of test accuracies.
+        """
+        if isinstance(self.optimizer, torch.optim.SGD):
+            mode = 'SGD'
         else:
-            self.optimizer = torch.optim.Adam(self.resnet50.parameters(), lr=0.01)        
-    
-    def train_test_split(self, test_size=0.2):
-        """ 
-        Loads the cifar10 data and splits into train and test sets.
+            mode = 'Adam'
+        print('-----------------------------------------------------------------------------------------------------------------------------------------------------------------')
+        print('| \t\t\t\t\t\t\t\t       Optimizer: ', mode)
+        print('-----------------------------------------------------------------------------------------------------------------------------------------------------------------')
+        print(f'| Epoch \t | Train Loss \t | Train Acc \t | Val Loss \t | Val Acc \t | Val F1-Score \t | Test Loss \t | Test Acc \t | Time') 
+        print('-----------------------------------------------------------------------------------------------------------------------------------------------------------------')
         
-        Args:
-            test_size (float): the proportion of the data to be used as the test set
-        """
-        self.train_loader, self.test_loader = load_cifar10_data(sampling_method=self.sampling_method, test_size=test_size)
-    
-    def random_split_validation(self, validation_size=0.1):
-        """ 
-        Splits the train set into train and validation sets.
-        Uses a random subset of the train set as the validation set.
+        for epoch in range(epochs):
+            start = time.time()
+            train_loss, train_acc = self.train_model(train_loader)
+            val_loss, val_acc, val_f1 = self.val_model(val_loader)
+            test_loss, test_acc = self.test_model(test_loader)
+            end = time.time()
 
-        Args:
-            validation_size (float): the proportion of the train set to be used as the validation set
-        """
-        # getting random indices
-        idx = np.random.randint(0, len(self.train_loader.dataset), int(len(self.train_loader.dataset)*validation_size))
-        
-        # creating validation and train sets
-        self.validation_loader = DataLoader(Subset(self.train_loader.dataset, idx))
-        self.train_loader = DataLoader(Subset(self.train_loader.dataset, np.setdiff1d(np.arange(len(self.train_loader.dataset)), idx)))
-        
-        # get batch size
-        self.batch_size = self.train_loader.batch_size
-    
-    def train(self, device, epochs=10):
-        """ 
-        Trains the model for a given number of epochs.
+            print(f'| \t {epoch+1} \t | {train_loss:.3f} \t | {train_acc:.3f}% \t |  {val_loss:.3f} \t | {val_acc:.3f}% \t | \t {val_f1:.3f} \t\t | {test_loss:.3f} \t | {test_acc:.3f}% \t | \t {(end-start) // 60} min(s)')
+            print('-----------------------------------------------------------------------------------------------------------------------------------------------------------------')
 
-        Args:
-            epochs (int): the number of epochs to train the model for
-        """
-        self.train_test_split()
-        self.random_split_validation()
-        self.resnet50.train()
-
-        print("==> Starting training...\n")
-        
-        print('\t\t\t\t Training Report')
-        print("|----------------------------------------------------------------------------------------------------------|")
-        print(f"| Epoch \t | Train Loss \t | Train Accuracy \t | Validation Loss \t | Validation Accuracy")
-        print("|----------------------------------------------------------------------------------------------------------|")
-        
-
-        # TODO add another metric during training other than loss and accuracy
-        
-        for i in range(epochs):
-            
-            train_loss = 0
-            n_correct_train = 0
-            
-            for data, target, target_mix, lam in self.train_loader:
-                data, target, target_mix, lam = data.to(device), target.to(device), target_mix.to(device), lam.to(device)
-                
-                self.optimizer.zero_grad()
-                
-                output = self.resnet50(data)
-                loss = self.mixup_loss(output, target, target_mix, lam)
-                loss.backward()
-                self.optimizer.step()
-
-                train_loss += loss.item()
-                
-                pred_train = output.argmax(dim=1, keepdim=True)
-                target_train = target.argmax(dim=1, keepdim=True)
-                
-                n_correct_train += torch.mean((pred_train == target_train).float()).item()
-            
-            val_loss = 0
-            n_correct_val = 0
-            
-            for data_val, target_val, target_mix_val, lam_val in self.validation_loader:
-                data_val, target_val, target_mix_val, lam_val = data_val.to(device), target_val.to(device), target_mix_val.to(device), lam_val.to(device)
-                
-                output_val = self.resnet50(data_val)
-                loss_val = self.mixup_loss(output_val, target_val, target_mix_val, lam_val)
-                
-                val_loss += loss_val.item()
-
-                pred_val = output_val.argmax(dim=1, keepdim=True)
-                target_val = target_val.argmax(dim=1, keepdim=True)
-
-                n_correct_val += torch.mean((pred_val == target_val).float()).item()
-
-            train_loss = (train_loss / self.batch_size ).item()
-            validation_loss = (val_loss / self.batch_size ).item()
-            
-            mean_correct_train = (n_correct_train / self.batch_size).item()
-            mean_correct_val = (n_correct_val / self.batch_size).item()
-            
-            print(f"| {i+1} \t | {train_loss:.4f} \t\t | {mean_correct_train:.4f} \t\t | {validation_loss:.4f} \t\t | {mean_correct_val:.4f}")
-        
-        print("|---------------------------------------------------------------------------------------------------------- \n")
+        print('\n')
 
 if __name__ == "__main__":
-    
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    ablation = Ablation()
+    print('\n')
+    train_loader, val_loader, test_loader = load_cifar10_train_val_test()
+    print('\n')
+    print('\t\t\t\t\t\t\t ########################################')
+    print(' \t\t\t\t\t\t\t\t      Ablation Report')
+    print('\t\t\t\t\t\t\t ########################################\n')
 
-    ablation_sgd = Ablation('sgd', sampling_method=1)
-    ablation_sgd.train(device=device, epochs=10)
-    ablation.test(ablation_sgd.test_loader, device=device, create_montage=False)
+    ablation_sgd = Ablation(num_classes=10, optimizer='sgd')
+    ablation_sgd.train(train_loader, val_loader, test_loader, epochs=10)
 
-    ablation_adam = Ablation('adam', sampling_method=1)
-    ablation_adam.train(device=device, epochs=10)
-    ablation.test(ablation_adam.test_loader, device=device, create_montage=False)
+    ablation_adam = Ablation(num_classes=10, optimizer='adam')
+    ablation_adam.train(train_loader, val_loader, test_loader, epochs=10)
+
+    torch.save(ablation_sgd.resnet50.state_dict(), f"{os.path.dirname(os.path.abspath(__file__))}/model_ablation_sgd.pt")
+    torch.save(ablation_adam.resnet50.state_dict(), f"{os.path.dirname(os.path.abspath(__file__))}/model_ablation_adam.pt")
+
+
